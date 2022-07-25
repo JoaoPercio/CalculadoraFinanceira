d5b2c70d277dd0e8e7e3d0f9fb35fca83950ef88
Luiz-Tomasi <netolt123@hotmail.com>
2022-07-25 18:50:49 -0300

    Implementado o java script a página da calculadora

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,61 @@
 function valorTotalInvestido (valorInicial, valorMensal, tempo){
-    if(tempo = mensal){
-        valorTotalInvestido = valorInicial + valorMensal*tempo;
-    } else {
-        valorTotalInvestido = valorInicial + valorMensal*tempo*12;
-    }
-    return valorTotalInvestido;
+    // recuperação de todos os valores informados na tela
+    window.valorTotalInvestido = 0;
+    //calculo
+    valorTotalInvestido = Number(valorMensal) * Number(tempo) + Number(valorInicial);
+    // setar todos os resultados
+    document.getElementById('valorTotalInvestidoP').innerHTML = valorTotalInvestido;
 }
-
-valorTotalJuros (10000, 1000, 10, 1);
 
 function valorTotalJuros (valorInicial, valorMensal, tempo, taxa){
     i = 0;
-    saldo = valorInicial;
-    if(tempo = mensal){
-       for(i == 0; i < tempo; i++) {
-            saldo = saldo * taxa + valorMensal + saldo;
-        }
-    valorTotalJuros = saldo - ( valorInicial + valorMensal * tempo ) ;
-    } else{
-        for(i == 0; i < tempo*12; i++) {
-            saldo = saldo * taxa + valorMensal + saldo;
-       }
-        valorTotalJuros = saldo - ( valorInicial + valorMensal * tempo* 12 ) ;
+    window.valorTotalJuros = 0;
+    let saldo = valorInicial;
+    for(i == 0; i <= tempo; i++) {
+        saldo = (Number(taxa) * Number(saldo)) + Number(saldo) + Number(valorMensal);
     }
+    console.log("Saldo:" + saldo);
+    console.log("valorTotalInvestido:" + valorTotalInvestido);
+    valorTotalJuros = saldo - valorTotalInvestido ;
     console.log(valorTotalJuros);
-    return valorTotalJuros;
+    document.getElementById('valorTotalJurosP').innerHTML = valorTotalJuros;
 }
 
 
-function impostoRenda (valorInicial, valorMensal, tempo, taxa) {
-    if (tempo = mensal){
+function impostoRenda (tempo) {
         dias = tempo * 30;
-    } else {
-        dias = tempo * 12 * 30;
+        window.impostoRenda = 0;
+    if( dias <= 180){
+        impostoRenda = valorTotalJuros * 0.225;
+    } if (dias >= 181 && dias <= 360) {
+        impostoRenda = valorTotalJuros * 0.2;
+    } if (dias >= 361 && dias <= 720){
+        impostoRenda = valorTotalJuros * 0.175;
+    } if (dias > 720){
+        impostoRenda = valorTotalJuros * 0.15;
     }
 
-    if( dias <= 180){
-        impostoRenda = valorTotalJuros(valorInicial, valorMensal, tempo, taxa) * 0.225;
-    } if (dias >= 181 && dias <= 360) {
-        impostoRenda = valorTotalJuros(valorInicial, valorMensal, tempo, taxa) * 0.2;
-    } if (dias >= 361 && dias <= 720){
-        impostoRenda = valorTotalJuros(valorInicial, valorMensal, tempo, taxa) * 0.175;
-    } if (dias > 720){
-        impostoRenda = valorTotalJuros(valorInicial, valorMensal, tempo, taxa) * 0.15;
-    }
+    document.getElementById('valorImpostoRendaP').innerHTML = impostoRenda;
 }
 
-function calculoRedimento(valorInicial, valorMensal, tempo, taxa){
-    rendimento = valorTotalInvestido (valorInicial, valorMensal, tempo) + valorTotalJuros (valorInicial, valorMensal, tempo, taxa) - impostoRenda (valorInicial, valorMensal, tempo, taxa) 
-    return rendimento;
+function calculoRedimento(){
+    rendimento = valorTotalInvestido + valorTotalJuros- impostoRenda;
+    document.getElementById('ValorTotalFinalP').innerHTML = rendimento;
+}
+
+function chamarCalculo(){
+    let valorInicial = document.getElementById('valorInicial').value;
+    let valorMensal = document.getElementById('valorMensal').value;
+    let taxa= document.getElementById('taxa').value;
+    let tempo = document.getElementById('tempo').value;
+
+    console.log("Valor Inicial:" + valorInicial);
+    console.log("Valor Mensal:" + valorMensal);
+    console.log("taxa:" + taxa);
+    console.log("tempo:" + tempo);
+    valorTotalInvestido(valorInicial, valorMensal, tempo);
+    valorTotalJuros (valorInicial, valorMensal, tempo, taxa);
+    impostoRenda (tempo);
+    calculoRedimento();
+
 }
