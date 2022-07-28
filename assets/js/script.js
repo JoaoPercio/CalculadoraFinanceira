40c291594983944be9157fe3f12b69fba16e9466
Luiz-Tomasi <netolt123@hotmail.com>
2022-07-27 23:19:21 -0300

    Formatação dos input

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,65 +8,36 @@ function valorTotalInvestido (valorInicial, valorMensal, tempo, tempoInvestido){
         valorTotalInvestidoR = Number(valorMensal) * Number(tempo) * 12 + Number(valorInicial);
     }
     // setar todos os resultados
-    document.getElementById('valorTotalInvestidoP').innerHTML = valorTotalInvestidoR;
+    document.getElementById('valorTotalInvestidoP').innerHTML =  deFloatParaMoeda.format(valorTotalInvestidoR);
 }
 
 function valorTotalJuros (valorInicial, valorMensal, tempo, taxa, tempoInvestido, tempoRendimento){
+    
     i = 0;
-    window.valorTotalJurosR = 0;
+    window.valorTotalJurosR = 0.00;
     let saldo = valorInicial;
+    rend= 0.0;
 
     if(tempoInvestido == 1){
-        if(tempoRendimento == 1){ // funcional | Rendimento mensal | Periodo Mensal
-            if(tempo >= 1){ //
-            for(i == 0; i <= tempo; i++) { 
-                saldo = (Number(taxa) * Number(saldo)) + Number(saldo) + Number(valorMensal);
+         // funcional | Rendimento mensal | Periodo Mensal
+         for(i == 0; i <= tempo; i++) { 
+                rend= taxa * saldo;
+               valorTotalJurosR= valorTotalJurosR + rend;
+               saldo= saldo + valorMensal+rend;
             } 
-            valorTotalJurosR = saldo - valorTotalInvestidoR ;
-            } else {  // funcional | Rendimento mensal | periodo Mensal | caso seja 0 ou menor
-                valorTotalJurosR = 0;
-            }
-        } else if(tempoRendimento == 2){ // funcional | Rendimento anual | Periodo Mensal
-            if(tempo >= 12){
-                quantDeAnos = tempo/12;
-                quantDeAnos = parseInt(quantDeAnos);
-                
-
-                for(i == 0; i <= quantDeAnos; i++) {
-                    saldo = Number(saldo) + Number(valorMensal)* 12;
-                    saldo = Number(taxa) * Number(saldo) + Number(saldo);
-                }
-                valorTotalJurosR = saldo - valorTotalInvestidoR ;
-            } else{ // funcional | Rendimento anual | periodo Mensal | caso seja 0 ou menor
-                valorTotalJurosR = 0;
-                //avisar que o rendimento está anual e não passou 1 ano?
-            }    
-        } 
+        
+           
 
     } else if (tempoInvestido == 2){ // periodo anual
-        if(tempoRendimento == 1){ 
-            if(tempo >= 1){// funcional | Rendimento mensal
-            for(i == 0; i <= tempo*12; i++) { 
-                saldo = (Number(taxa) * Number(saldo)) + Number(saldo) + Number(valorMensal);
+        for(i == 0; i <= tempo*12; i++) { 
+            rend= taxa * saldo;
+            valorTotalJurosR= valorTotalJurosR + rend;
+            saldo= saldo + valorMensal+rend;
             } 
-            valorTotalJurosR = saldo - valorTotalInvestidoR ;
-            } else { // funcional | Rendimento mensal | caso seja 0 ou menor
-                    valorTotalJurosR = 0;
-            }
-        } else if(tempoRendimento == 2){ // funcional |  Rendimento anual
-            if(tempo >= 1){
-                for(i == 0; i < tempo; i++) {
-                    saldo = Number(saldo) + Number(valorMensal)* 12;
-                    saldo = Number(taxa) * Number(saldo) + Number(saldo);
-                }
-                valorTotalJurosR = saldo - valorTotalInvestidoR ;
-            } else { // funcional | Rendimento anual | caso seja 0 ou menor
-                valorTotalJurosR = 0;
+        
+           
         }
-        }
-    }
-
-    document.getElementById('valorTotalJurosP').innerHTML = valorTotalJurosR;
+    document.getElementById('valorTotalJurosP').innerHTML =  deFloatParaMoeda.format(valorTotalJurosR);
 }
 
 
@@ -79,42 +50,123 @@ function impostoRenda (tempo, tempoInvestido) {
         
         window.impostoRendaR = 0;
     if( dias <= 180){
-        impostoRendaR = valorTotalJurosR * 0.225;
+        impostoRendaR = valorTotalJurosR * (22.5/100);
     } if (dias >= 181 && dias <= 360) {
-        impostoRendaR = valorTotalJurosR * 0.2;
+        impostoRendaR = valorTotalJurosR * (20/100);
     } if (dias >= 361 && dias <= 720){
-        impostoRendaR = valorTotalJurosR * 0.175;
+        impostoRendaR = valorTotalJurosR * (17.5/100);
     } if (dias > 720){
-        impostoRendaR = valorTotalJurosR * 0.15;
+        impostoRendaR = valorTotalJurosR * (15/100);
     }
 
-    document.getElementById('valorImpostoRendaP').innerHTML = impostoRendaR;
+    document.getElementById('valorImpostoRendaP').innerHTML =  deFloatParaMoeda.format(impostoRendaR);
 }
 
 function calculoRedimento(){
     rendimento = valorTotalInvestidoR + valorTotalJurosR - impostoRendaR;
-    document.getElementById('ValorTotalFinalP').innerHTML = rendimento;
+    document.getElementById('ValorTotalFinalP').innerHTML = deFloatParaMoeda.format(rendimento);
 }
 
 function chamarCalculo(){
     // pega os valores dos inputs
     let valorInicial = document.getElementById('valorInicial').value;
+    valorInicial=viraNormal(valorInicial)
     let valorMensal = document.getElementById('valorMensal').value;
+    valorMensal=viraNormal(valorMensal)
     let taxa= document.getElementById('taxa').value;
+    taxa=viraNormal(taxa)
     let tempo = document.getElementById('tempo').value;
 
 
-    select = document.getElementById('inputGroupSelect01');
-    let tempoRendimento = select.options[select.selectedIndex].value;
     select = document.getElementById('inputGroupSelect02');
     let tempoInvestido = select.options[select.selectedIndex].value;
 
 
     valorTotalInvestido(valorInicial, valorMensal, tempo, tempoInvestido);
-    valorTotalJuros (valorInicial, valorMensal, tempo, taxa, tempoInvestido, tempoRendimento);
+    valorTotalJuros (valorInicial,valorMensal, tempo, taxa/100, tempoInvestido);
     impostoRenda (tempo, tempoInvestido);
     calculoRedimento();
 
-    console.log("TempoRendimento: " + tempoRendimento);
     console.log("TempoInvestido: " + tempoInvestido);
+}
+function drawChart() {
+
+    var data = new google.visualization.DataTable();
+    data.addColumn('number', 'Investimento');
+    data.addColumn('number', 'rendimento');
+    data.addColumn('number', 'Investimento Total');
+   
+
+    data.addRows([
+      [1,  37.8, 1 ],
+      [2,  30.9, 2],
+      [3,  25.4, 3],
+      [4,  11.7, 4],
+      [5,  11.9, 5],
+      [6,   8.8, 6],
+      [7,   7.6, 7],
+      [8,  12.3, 8],
+      [9,  16.9,9],
+      [10, 12.8, 10],
+      [11,  5.3, 11 ],
+      [12,  6.6, 12],
+      [13,  4.8, 13 ],
+      [14,  4.2, 14]
+    ]);
+
+    var options = {
+      chart: {
+        title: 'Investimento',
+        
+      },
+      width: 600,
+      height: 500,
+      axes: {
+        x: {
+          0: {side: 'top'}
+        }
+      }
+    };
+
+    var chart = new google.charts.Line(document.getElementById('line_top_x'));
+
+    chart.draw(data, google.charts.Line.convertOptions(options));
+  }
+
+
+function formatarMoeda(moeda) {
+    var elemento = moeda;
+    var valor = elemento.value;
+
+    valor = valor + '';
+    valor = parseInt(valor.replace(/[\D]+/g, ''));
+    valor = valor + '';
+    valor = valor.replace(/([0-9]{2})$/g, ",$1");
+
+    if (valor.length > 6) {
+        valor = valor.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
+    }
+
+    elemento.value = valor;
+    if(valor == 'NaN') elemento.value = '';
+}
+function viraNormal(variable){
+    if(variable ===""){
+        variable=0;
+    }
+    variable = variable.replace("R$", "")
+    variable = variable.replace(/\./g, "")
+    variable = variable.replace(/\,/g,".")
+    return parseFloat(variable)
+}
+var deFloatParaMoeda = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+    minimumFractionDigits: 2,
+  });
+function limpar(){
+    document.getElementById('ValorTotalFinalP').innerHTML = "";
+    document.getElementById('valorImpostoRendaP').innerHTML = "";
+    document.getElementById('valorTotalJurosP').innerHTML =  "";
+    document.getElementById('valorTotalInvestidoP').innerHTML = "";
 }
